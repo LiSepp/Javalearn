a4f7e859ee27942a55af66c60327f212d0fd9357
LiSepp <yaoye084@gmail.com>
2018-03-11 15:50:45 +0800

    增加了一些注释和标签

--- thinking/HelloDate.java/src/HelloDate.java
+++ thinking/HelloDate.java/src/HelloDate.java
@@ -1,6 +1,17 @@
+//:c02:HelloDate.java
 import java.util.*;
+/**The first Thinking in Java example prigram.
+*Displays a string and today's date.
+*@author LiSepp
+*@author yaoye084@gmail.com
+*@version 2.0
+*/
 public class HelloDate {
-
+/**Sole entry point to class & application
+ * @param args array of string arguments
+ * @return No return value
+ * @exception exceptions No exceptions thrown
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,it's:");
@@ -8,4 +19,4 @@ public class HelloDate {
 
 	}
 
-}
+}///~
