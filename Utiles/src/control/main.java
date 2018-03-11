4eff89bbd96a50cb3424b29a591e689376b59a8b
LiSepp <yaoye084@gmail.com>
2018-03-11 14:17:18 +0800

    不依靠书使用eclipse建立project,package和class

---
+++ Utiles/src/control/main.java
@@ -0,0 +1,18 @@
+package control;
+
+public class main {
+
+	public static void main(String[] args) {
+		//ctrl+1 快捷修
+		//ctrl+D 快捷删除行
+		//shift+Enter创建新的行（不必滚轮到每行最后一个）
+		//ctrl+F11快速运行项目
+		//alt+上下键 快速移动行
+		//ctrl+m放大工作区
+		//alt+/补全代码
+		System.out.println("Run2");
+		int a = (int) 100L;
+		System.out.println("main.main()");
+	}
+
+}
