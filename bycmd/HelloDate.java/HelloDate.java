c4a8b3e0d6c9afae0ea47a728c6d06a8dfb67243
LiSepp <yaoye084@gmail.com>
2018-03-13 09:57:47 +0800

    练手文件夹，由sublime完成

---
+++ bycmd/HelloDate.java/HelloDate.java
@@ -0,0 +1,9 @@
+//c02HelloDate.java
+import java.util.*;
+
+public class HelloDate {
+	public static void main(String[] args) {
+		System.out.println("Hello, is's: ");
+		System.out.println(new Date());
+	}
+}
---
+++ bycmd/HelloDate.java/HelloWorld_practice.java
@@ -0,0 +1,78 @@
+public class Helloworld {
+	public static void main (string[] args) {
+		system.out.println("Hello,World");
+	}
+
+}
+
+public class Helloworld {
+	public static void main (string[] args){
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main (string[] args){
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main (string[] args){
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main (string[] args){
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string[] args) {
+		system.out.println("Hello,World");
+	}
+}
+
+public class HellowWorld {
+	public static void main(string[] args){
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string[] args){
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string[] args) {
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string[] args) {
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string[] args) {
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string [] args) {
+		system.out.println("Hello,World");
+	}
+}
+
+public class HelloWorld {
+	public static void main(string[] args) {
+		system.out.println("Hello,World");
+	}
+}
