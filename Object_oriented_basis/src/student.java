68f245b125387c1bd1353156daf49aed8ac84193
LiSepp <yaoye084@gmail.com>
2018-03-21 12:57:16 +0800

    面向对象基础

---
+++ Object_oriented_basis/src/student.java
@@ -0,0 +1,17 @@
+
+public class student {
+	//
+	public String sno; //学号
+	public String sname; //姓名
+	//
+	//定义方法
+	public String getName(String name)
+	{
+		//  方法体
+	}
+	Object oriented basis
+}
+
+/*
+ * 使用class定义了一个名为student类的格式
+*/
