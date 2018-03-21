10ae205f98c526b0684d00d839822e8bbac3d1f5
LiSepp <yaoye084@gmail.com>
2018-03-21 13:07:39 +0800

    对象的创建和使使用

--- Object_oriented_basis/src/student.java
+++ Object_oriented_basis/src/student.java
@@ -1,17 +1,24 @@
 
-public class student {
+class student //定义名为StudentDome的类
+{
 	//
 	public String sno; //学号
 	public String sname; //姓名
-	//
-	//定义方法
-	public String getName(String name)
+	
+class StudentDemo //定义名为StudentDome的类
+	public static void main(String args[])
 	{
-		//  方法体
+		Student s1 = new Student (); //声明一个Student类型的对象s1
+		s1.sno = "B15011515";
+		s1.sname ="LiSepp";
+		System.out.println("The student is " + s1. sno +s1.sname);
 	}
-	Object oriented basis
+
 }
 
-/*
- * 使用class定义了一个名为student类的格式
+/*Student StudentDome是类
+ * s1是对象
+ * sno sname是类中声明的对象，称为成员对象，访问类的成员对象，需要使用.运算符
+ */
+ * 
 */
